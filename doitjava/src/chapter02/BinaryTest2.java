65f44409717da367820ba9828e5d3e7e1cba03c1
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-08-12 16:29:36 +0900

    chapter02 - 진수사용법

---
+++ doitjava/src/chapter02/BinaryTest2.java
@@ -0,0 +1,17 @@
+package chapter02;
+
+public class BinaryTest2 {
+
+	public static void main(String[] args) {
+		int bNum = 0B10100;	//2진수
+		int dNum = 20;		//10진수
+		int oNum = 024;		//8진수
+		int hNum = 0x14;	//16진수
+		
+		System.out.println(bNum);
+		System.out.println(dNum);
+		System.out.println(oNum);
+		System.out.println(hNum);
+	}
+
+}
