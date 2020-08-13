a6cf9650896476e4ccd696669e786ce87a453fe3
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-08-13 17:15:41 +0900

    chapter02 - 변수 사용법

---
+++ doitjava/src/chapter02/Variable2.java
@@ -0,0 +1,20 @@
+package chapter02;
+
+public class Variable2 {
+
+	public static void main(String[] args) {
+		int level = 150;
+		int mp;
+		System.out.println("현재 레벨은 몇인가요?");
+		System.out.println(level);
+		System.out.println("현재 마력은 몇인가요?");
+		mp = 1000;
+		System.out.println(mp);
+		System.out.println("마법을 사용하여 마력이 닳았습니다.");
+		mp =300;
+		System.out.println("현재 마력은 몇인가요?");
+		System.out.println(mp);
+		
+	}
+
+}
