bf1fba3025185329339214ec9888792d6519e9c0
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-08-14 13:22:34 +0900

    chapter02 - 실수연산의 오류

---
+++ doitjava/src/chapter02/DoubleEx2.java
@@ -0,0 +1,11 @@
+package chapter02;
+
+public class DoubleEx2 {
+
+	public static void main(String[] args) {
+		double value; //double 변수 선언
+		value = (1.0e20 + 5) - 1.0e20; 
+		System.out.println(value);
+		
+	}
+}
