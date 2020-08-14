eb483fb49e376448cebf80a533793b211d9da14d
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-08-14 14:55:08 +0900

    chapter02 - 명시적형변환(강제형변환)

---
+++ doitjava/src/chapter02/CastingEx.java
@@ -0,0 +1,23 @@
+package chapter02;
+
+public class CastingEx {
+
+	public static void main(String[] args) {
+		int iVal = 44032;
+		char cVal = (char) iVal;
+		System.out.println(cVal);
+		
+		long lVal = 500L;
+		iVal = (int) lVal;
+		System.out.println(iVal);
+		
+		double dVal = 3.14;
+		iVal = (int) dVal;
+		System.out.println(iVal);
+		
+		int iNum = 103_029_770;
+		byte bNum = (byte) iNum;
+		System.out.println(bNum);
+	}
+
+}
