6a5f7fd6ebe0dd2c17d814a28f33ab922ee59bc8
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-08-14 13:01:02 +0900

    chapter02 - 실수 자료형 예제

---
+++ doitjava/src/chapter02/DoubleEx1.java
@@ -0,0 +1,14 @@
+package chapter02;
+
+public class DoubleEx1 {
+
+	public static void main(String[] args) {
+		double dNum = 3.14123456789012345; //소수점 16자리
+		float fNum = 3.14123456789012345f; //소수점 7자리
+		
+		System.out.println(dNum);
+		System.out.println(fNum);
+		
+	}
+
+}
