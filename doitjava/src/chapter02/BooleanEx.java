497c92c0ded8482136a2dc693954de86b11b47af
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-08-14 13:43:08 +0900

    chapter02 -블린 예제

---
+++ doitjava/src/chapter02/BooleanEx.java
@@ -0,0 +1,11 @@
+package chapter02;
+
+public class BooleanEx {
+
+	public static void main(String[] args) {
+		boolean isMarried = false; //boolean 변수를 선언하고 초기화
+		System.out.println(isMarried);
+		
+	}
+
+}
