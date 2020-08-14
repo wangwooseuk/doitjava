f08d86e65cc2534a63cddfefeb1b61f06cb21226
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-08-14 15:45:00 +0900

    chapter03 - 산술연산자

---
+++ doitjava/src/chapter03/ArithmeticOperator.java
@@ -0,0 +1,26 @@
+package chapter03;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		int iRes;
+		double dRes;
+		
+		iRes = 3 + 2;
+		System.out.println(iRes);
+		iRes = 3 - 2;
+		System.out.println(iRes);
+		iRes = 3 * 2;
+		System.out.println(iRes);
+		iRes = 3 / 2;
+		System.out.println(iRes);
+		dRes = 3.0 / 2.0;
+		System.out.println(dRes);
+		iRes = 3 % 2;
+		System.out.println(iRes);
+		
+		dRes = (double) 3 / 2;
+		System.out.println(dRes);
+	}
+
+}
