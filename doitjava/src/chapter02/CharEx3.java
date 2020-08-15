bfdf42d091acebb0b9794eb5d61f93127c5d0ce9
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-08-15 13:24:12 +0900

    chapter02 - 문자형 변수 사용법

---
+++ doitjava/src/chapter02/CharEx3.java
@@ -0,0 +1,22 @@
+package chapter02;
+
+public class CharEx3 {
+
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		int iNum = ch1;
+		System.out.println(iNum);
+		
+		char ch2 = 'D';
+		iNum = ch2;
+		System.out.println(iNum);
+		
+		char ch3 = 69;
+		char ch4 = 70;
+		System.out.println(ch3);
+		System.out.println(ch4);	
+		
+		int res = 'F'-'A';
+		System.out.println(res);
+	}
+}
