9016cb155c0afde6697bc48e8231089517b288cb
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-08-26 16:41:32 +0900

    example - 복습문제

--- doitjava/src/example/Exam05.java
+++ doitjava/src/example/Exam05.java
@@ -1,24 +1,24 @@
 package example;
 
 /*
- * 1에서 10까지의 수 중 짝수의 합계를
+ * 1에서 10까지의 수 중 홀수의 합계를
  * 구하는 프로그램을 작성하세요.
  */
 
-public class Exam04 {
+public class Exam05 {
 
 	public static void main(String[] args) {
 		int sum = 0;
 		
-//		for(int i = 1; i <= 10; i++) {
-//			if(i % 2 == 0) {
+//		for(int i=1; i<=10; i++) {
+//			if(i % 2 != 0) {
 //				sum += i;
 //			}
 //		}
 //		System.out.println(sum);
 		
-		for(int i = 1; i <= 10; i++) {
-			if(i % 2 != 0) {
+		for(int i=1; i<=10; i++) {
+			if(i % 2 == 0) {
 				continue;
 			}
 			sum += i;
