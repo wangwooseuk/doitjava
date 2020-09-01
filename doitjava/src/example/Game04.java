3fa686de82f25efc0674e42ee70bd7d689afb923
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-09-01 15:55:31 +0900

    example - 소수문제

---
+++ doitjava/src/example/Game04.java
@@ -0,0 +1,25 @@
+package example;
+
+import java.util.Scanner;
+
+/*
+ * 1부터 100 사이의 소수를 나타내라.
+ */
+
+public class Game04 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int user = 0;
+		
+		System.out.print("숫자를 입력하세요: ");
+		
+		int com = (int) (Math.random() * 100) + 1;
+		user = sc.nextInt();
+		
+			
+		
+
+	}
+}
