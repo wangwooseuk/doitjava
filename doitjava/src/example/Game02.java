486b844a8c1d32f5a5b6d7fe215a515387f27d43
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-09-01 15:53:44 +0900

    example - 숫자맞추기

--- doitjava/src/example/Game02.java
+++ doitjava/src/example/Game02.java
@@ -1,29 +1,50 @@
 package example;
 
+import java.util.Scanner;
+
 public class Game02 {
 
 	public static void main(String[] args) {
-		int com = 75;
+		Scanner sc = new Scanner(System.in);
+		
+		int com = (int) (Math.random() * 100) + 1;
+		int user = 0;
 		int cnt = 0;
-		System.out.println("생성된 숫자는 무엇인가요? ");
-		System.out.print("> ");
-
+		
+		System.out.println("숫자 맞추기 게임을 시작합니다.");
+		System.out.println("1 ~ 100 중에 랜덤하게 생성된 숫자를 맞춰 주세요. ");
+		
 		while (true) {
-			com = (int) (Math.random() * 100);
-			System.out.println(com);
-			
-			if (com < 75) {
-				System.out.println("High(up)");
+			System.out.print("> ");
+			user = sc.nextInt();
+			System.out.println((++cnt) + "회 입력하셨습니다.");
+			if (user > com) {
+				System.out.println("입력하신 숫자보다 아래에 있습니다.(Down!)");
+			} else if (user < com) {
+				System.out.println("입력하신 숫자보다 위에 있습니다.(Up!)");
 			} else {
-				if (com == 75) {
-					System.out.println("정답");
-					break;
-				} else {
-					System.out.println("Low(down)");
-				}
+				System.out.println("정답입니다. 게임을 종료합니다.");
+				sc.close();
+				break;
 			}
-			cnt++;
-			System.out.println(cnt + "회");
 		}
+
+//		while (true) {
+//			com = (int) (Math.random() * 100) + 1;
+//			System.out.println(com);
+//
+//			if (com < 75) {
+//				System.out.println("High(up)");
+//			} else {
+//				if (com == 75) {
+//					System.out.println("정답");
+//					break;
+//				} else {
+//					System.out.println("Low(down)");
+//				}
+//			}
+//			cnt++;
+//			System.out.println(cnt + "회");
+//		}
 	}
 }
