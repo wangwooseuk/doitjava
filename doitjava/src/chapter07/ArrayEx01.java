ef2af7cd47763d6a305a7fd370279503c9431f89
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-09-03 16:02:29 +0900

    chapter07 - 배열

---
+++ doitjava/src/chapter07/ArrayEx01.java
@@ -0,0 +1,32 @@
+package chapter07;
+
+public class ArrayEx01 {
+
+	public static void main(String[] args) {
+		int score1 = 1, score2 = 2, score3 = 3, score4 = 4, score5 = 5, score6 = 6, score7 = 7, score8 = 8, score9 = 9,
+				score10 = 10;
+		int sum = 0;
+		sum += score1;
+		sum += score2;
+		sum += score3;
+		sum += score4;
+		sum += score5;
+		sum += score6;
+		sum += score7;
+		sum += score8;
+		sum += score9;
+		sum += score10;
+		System.out.println(sum);
+
+		sum = 0;
+		int[] scores = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+		// 배열의 초기화는 중괄호{}로 할수 있고
+		// 모든 언어 공통 명령어
+		// int[] scores = int scores[] 둘다 가능, 섞어서 사용도 가능
+		System.out.println("scores의 배열의 개수: " + scores.length);
+		for (int i = 0; i < scores.length; i++) {
+			sum += scores[i];
+		}
+		System.out.println(sum);
+	}
+}
