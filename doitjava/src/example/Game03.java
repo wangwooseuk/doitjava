f33544a5f619dd2b7241b0c20c06378bb7bb6d89
wang woo seuk <wangwooseuk.dev@gmail.com>
2020-09-04 14:42:01 +0900

    example - switch문제

--- doitjava/src/example/Game03.java
+++ doitjava/src/example/Game03.java
@@ -15,7 +15,13 @@ public class Game03 {
 		int month = sc.nextInt();
 
 		switch (month) {
-		case 1, 3, 5, 7, 8, 10, 12:
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
 			day = 31;
 			break;
 		case 2:
