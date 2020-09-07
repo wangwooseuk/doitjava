b03756e6db6df63928b825f0a9fab8ac22d4adcf
wang woo seuk <wangwooseuk.dev@gmail.com>
2020-09-07 17:29:48 +0900

    chapter07 - 이차원배열

--- doitjava/src/chapter07/ArrayEx08.java
+++ doitjava/src/chapter07/ArrayEx08.java
@@ -3,13 +3,11 @@ package chapter07;
 public class ArrayEx08 {
 	public static void main(String[] args) {
 		int[] arr1 = { 1, 2, 3 };
-		int[] arr2 = { 4, 5, 6 };
-		int[] arr3 = { 7, 8, 9 };
+		int[] arr2 = { 4, 5, 6, 7, 8, 9 };
 
-		int[][] array = new int[3][];
+		int[][] array = new int[][] { { 50, 60, 70 }, { 30, 40, 20 }, { 10, 90, 80 } };
 		array[0] = arr1;
 		array[1] = arr2;
-		array[2] = arr3;
 
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array[i].length; j++) {
