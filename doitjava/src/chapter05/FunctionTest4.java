5ca4172dde9717d266b303f6ac1fd0fa53a0ebca
wang woo seuk <wangwooseuk.dev@gmail.com>
2020-09-09 16:44:48 +0900

    chapter05 - 생성자,add메소드

---
+++ doitjava/src/chapter05/FunctionTest4.java
@@ -0,0 +1,12 @@
+package chapter05;
+
+public class FunctionTest4 {
+	public static void main(String[] args) {
+		Operator o = new Operator();
+		System.out.println(o.add(1, 2));
+		System.out.println(o.add(10, 20));
+		System.out.println(o.add(30, 20));
+		o.printStar();
+		
+	}
+}
