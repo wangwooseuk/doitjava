5442ee30e0f0579d20c4fbdc07ed2c484adbd4d9
wang woo seuk <wangwooseuk.dev@gmail.com>
2020-09-10 17:27:47 +0900

    chapter05 - private.get-set

---
+++ doitjava/src/chapter05/Car.java
@@ -0,0 +1,58 @@
+package chapter05;
+
+public class Car {
+	private String model;
+	private String color;
+	private int gear;
+	private int speed;
+	private int maxSpeed;
+	
+	void info() {
+		System.out.println(model + ", " + color);
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getGear() {
+		return gear;
+	}
+
+	public void setGear(int gear) {
+		this.gear = gear;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		if(speed < 0) {
+			this.speed = 0;
+		} else {
+			this.speed = speed;
+		}
+	}
+
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+	
+}
---
+++ doitjava/src/chapter05/CarEx.java
@@ -0,0 +1,11 @@
+package chapter05;
+
+public class CarEx {
+	public static void main(String[] args) {
+		Car c1 = new Car();
+		c1.setModel("그랜저");
+		c1.setColor("검정");
+		
+		c1.info();
+	}
+}
