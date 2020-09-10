fd47b041a3431f074db5ed4fc5d15359b0d22bbe
wang woo seuk <wangwooseuk.dev@gmail.com>
2020-09-10 17:31:19 +0900

    chapter05 - 메소드

---
+++ doitjava/src/chapter05/CalcOverEx.java
@@ -0,0 +1,17 @@
+package chapter05;
+
+public class CalcOverEx {
+	public static void main(String[] args) {
+		CalcOver myCalcu = new CalcOver();
+		
+		//정사각형 넓이 구하기
+		double result1 = myCalcu.areaRectangle(10);
+		
+		//직사각형 넓이 구하기
+		double result2 = myCalcu.areaRectangle(10, 20);
+		
+		//결과 출력
+		System.out.println("정사각형 넓이 : " + result1);
+		System.out.println("직사각형 넓이 : " + result2);
+	}
+}
