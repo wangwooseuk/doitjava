19b1db7390321e3b13dbe42b272eee634a77b42f
wang woo seuk <wangwooseuk.dev@gmail.com>
2020-09-10 17:32:43 +0900

    chapter05 - 필드 메소드

--- doitjava/src/chapter05/Person.java
+++ doitjava/src/chapter05/Person.java
@@ -6,4 +6,33 @@ public class Person {
 	double weight;
 	char gender;
 	boolean married;
+	
+	public Person(String name, int height, double weight, char gender, boolean married) {
+		this.name = name;
+		this.height = height;
+		this.weight = weight;
+		this.gender = gender;
+		this.married = married;
+	}
+	
+	public Person() {
+		this.name = "ȫ�浿";
+		this.height = 180;
+		this.weight = 75.5;
+		this.gender = 'M';
+		this.married = false;
+	}
+	
+	void printName() {
+		System.out.println(this.name);
+	}
+	
+	void printHeight() {
+		System.out.println(height);
+	}
+	
+	void info()	{
+		printName();
+		printHeight();
+	}
 }
