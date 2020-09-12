5662749d975f6021f92c9ff0c41154ba5b1e3c50
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-09-12 12:10:15 +0900

    chapter08 - 상속

--- doitjava/src/chapter08/ChildEx.java
+++ doitjava/src/chapter08/ChildEx.java
@@ -4,8 +4,15 @@ public class ChildEx {
 	public static void main(String[] args) {
 		Child child = new Child();
 		
+		Parent parent = child;
+		parent.method1();
+		parent.method2();
+		//parent.method3() 호출 불가능
+		
+		System.out.println();
+		
 		child.method1();
 		child.method2();
-		//parent.method3() 호출 불가능
+		child.method3();
 	}
 }
