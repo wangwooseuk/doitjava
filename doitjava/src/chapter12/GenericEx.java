851982cb8f3becb55b8a0a5e98e52b34f60def60
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-09-19 13:27:12 +0900

    chapter12 - 제네릭 오토,언박싱

---
+++ doitjava/src/chapter12/GenericEx.java
@@ -0,0 +1,22 @@
+package chapter12;
+
+import java.util.ArrayList;
+
+public class GenericEx {
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList(); //객체만 입력가능
+		list.add(100);		//숫자
+		list.add(200);		//숫자
+		list.add("300");	//문자열
+		
+		System.out.println(list);
+		System.out.println();
+		
+		ArrayList<Integer> list2 = new ArrayList<Integer>(); //객체만 입력가능
+		list2.add(Integer.valueOf(100));		//숫자
+		list2.add(Integer.valueOf(200));		//숫자
+		list2.add(Integer.valueOf("300"));		//문자열
+		
+		System.out.println(list2);
+	}
+}
