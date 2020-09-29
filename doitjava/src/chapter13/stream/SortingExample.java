8be083c91f0c13c53811c28c68399b52aae470a8
wang woo seuk <wangwooseuk.dev@gmail.com>
2020-09-29 14:31:42 +0900

    chapter13 - 스트림(sort)

---
+++ doitjava/src/chapter13/stream/SortingExample.java
@@ -0,0 +1,32 @@
+package chapter13.stream;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class SortingExample {
+	public static void main(String[] args) {
+		//숫자 요소일 경우
+//		IntStream intStream = Arrays.stream(new int[] {5, 3, 2, 1, 4});
+		IntStream intStream = IntStream.of(new int[] {5, 3, 2, 1, 4});
+		intStream.sorted().forEach(n -> System.out.print(n +","));
+		System.out.println();
+		
+		//객체 요소일 경우
+		List<Student> studentList = Arrays.asList(
+				new Student("홍길동", 30), 
+				new Student("신용권", 10),
+				new Student("유미선", 20)
+		);
+		studentList.stream().sorted().forEach(s -> System.out.print(s.getScore() + ","));
+		System.out.println();
+		studentList.stream().sorted().forEach(s -> System.out.println(s));
+		System.out.println();
+		studentList.stream().sorted().forEach(System.out :: println);
+		System.out.println();
+		
+		studentList.stream()
+		.sorted(Comparator.reverseOrder()).forEach(s -> System.out.print(s.getScore() + ","));
+	}
+}
--- doitjava/src/chapter13/stream/Student.java
+++ doitjava/src/chapter13/stream/Student.java
@@ -1,6 +1,6 @@
 package chapter13.stream;
 
-public class Student {
+public class Student implements Comparable<Student> {
 		private String name;
 		private int score;
 		
@@ -19,4 +19,10 @@ public class Student {
 		public String toString() {
 			return name + "-" + score;
 		}
+		
+		@Override // Comparable 인터페이스의 추상 메소드
+		public int compareTo(Student o) {
+			return Integer.compare(score, o.score); //매개값을 비교하여 -1,0,1을 반환
+		}
+		
 }
