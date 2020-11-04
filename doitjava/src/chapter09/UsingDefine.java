1eac6979291bee9fcbed7bc97558e3bf53d9b5fe
WangWooSeuk <wangwooseuk.dev@gmail.com>
2020-11-04 21:32:52 +0900

    chapter09 - final 예제

---
+++ doitjava/src/chapter09/UsingDefine.java
@@ -0,0 +1,12 @@
+package chapter09;
+
+public class UsingDefine {
+	public static void main(String[] args) {
+		System.out.println(Define.GOOD_MORNING);
+		System.out.println("최소값은 " + Define.MIN + "입니다.");
+		System.out.println("최대값은 " + Define.MAX+ "입니다.");
+		System.out.println("수학 코드 값은 " + Define.ENG + "입니다.");
+		System.out.println("영어 코드 값은 " + Define.MATH + "입니다.");
+		System.out.println("파이 값은 " + Define.PI + "입니다.");
+	}
+}
